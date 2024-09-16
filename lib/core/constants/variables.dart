164a0e0e1c79fc3ef49ec482eb2822247fa72138
Erlangga <96732214+erlanggafs@users.noreply.github.com>
2024-09-16 15:17:39 +0700

    Update variables.dart

--- lib/core/constants/variables.dart
+++ lib/core/constants/variables.dart
@@ -1,5 +1,5 @@
 class Variables {
   static const String appName = 'Resto App';
   static const String apiVersion = 'v1';
-  static const String baseUrl = 'https://manageresto.my.id';
+  static const String baseUrl = 'https://api-backend-resto';
 }
